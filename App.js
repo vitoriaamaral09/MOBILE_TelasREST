3026b4dee14573d8da94e258e728ddea418615f4
Vitória Do Amaral Viana <vitoria.366912@alunosatc.edu.br>
2025-10-20 20:34:36 -0300

    feat: implementa a navegacao inicial e corrige erros

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Tela Inicial" component={HomeScreen} />
+        <Stack.Screen name="Detalhes" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="ir para detalhes"
+        onPress={() => navigation.navigate('Detalhes')}
+      ></Button>
+    </View>
+  );
+
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Voltar para Home"
+        onPress={() => navigation.navigate('Tela Inicial')}
+      ></Button>
+      <Button
+        title="Navegar para home"
+        onPress={() => navigation.navigate('Tela Inicial')}
+      ></Button>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
